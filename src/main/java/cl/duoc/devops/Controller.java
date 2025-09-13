70a5545bc2819defed483476450acb79ce34c882
Rodrigo Gallardo <167040497+ragallardom@users.noreply.github.com>
2025-09-13 02:04:53 -0300

    Se agrega nuevo endpoint en Controller.java para entregar fecha actual

--- src/main/java/cl/duoc/devops/Controller.java
+++ src/main/java/cl/duoc/devops/Controller.java
@@ -4,17 +4,28 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @RestController
 public class Controller {
 
+    private static final DateTimeFormatter HORA_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter FECHA_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
     public String hola() {
         String ahora = LocalDateTime.now().format(FMT);
         return "Hola Mundo\nLa hora en este momento es: " + ahora;
+    }
+
+    @GetMapping("/fecha")
+    public String getFecha() {
+        LocalDate today = LocalDate.now();
+        return "La fecha de hoy es: " + today.format(FECHA_FORMATTER);
     }
 }
